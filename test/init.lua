e6eef3a02435a877e7304aaeaf615e38af9efe75
Federico Simonetta <fsimonetta@iccmu.es>
2023-07-27 16:07:13 +0200

    Added test config

---
+++ test/init.lua
@@ -0,0 +1,50 @@
+local lazypath = vim.fn.stdpath("run") .. "/visual.nvim-test/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- Example using a list of specs with the default options
+vim.g.mapleader = " " -- Make sure to set `mapleader` before lazy so your mappings are correct
+
+require("lazy").setup({
+	{ "00sapo/visual.nvim" },
+	{
+		"nvim-treesitter/nvim-treesitter",
+		build = ":TSUpdate",
+		config = function()
+			local configs = require("nvim-treesitter.configs")
+
+			configs.setup({
+				ensure_installed = { "lua" }, -- add here the languages for your test case
+				sync_install = false,
+				highlight = { enable = true },
+				indent = { enable = true },
+				textobjects = {
+					select = {
+						enable = true,
+						lookahead = true,
+						keymaps = {
+							["af"] = "@function.outer",
+							["if"] = "@function.inner",
+							["ac"] = "@class.outer",
+							["ic"] = "@class.inner",
+							["aa"] = "@class.outer",
+							["ia"] = "@class.inner",
+						},
+					},
+				},
+			})
+		end,
+	},
+	{ "nvim-treesitter/nvim-treesitter-textobjects" },
+})
+
+vim.cmd"hi Visual guifg=black guibg=lightyellow ctermfg=black ctermbg=lightyellow"
